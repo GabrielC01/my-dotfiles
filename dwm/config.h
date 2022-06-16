26fe3ca6867b170e531b9342869eee6e69d5fad9
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-06-16 13:56:53 -0300

    Added brightness adjustment key bindings.

--- dwm/config.h
+++ dwm/config.h
@@ -66,10 +66,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *dec_brightness[] = { "brightnessctl", "s", "5%-", NULL };
+static const char *inc_brightness[] = { "brightnessctl", "s", "+5%", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_F11,    spawn,          {.v = dec_brightness } },
+	{ MODKEY,                       XK_F12,    spawn,          {.v = inc_brightness } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
