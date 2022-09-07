ed95c55bfbd53628e65885745d28b87b46d5780e
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-09-07 11:30:43 -0300

    Update.

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -8,4 +8,18 @@ vim.opt.background = "light"
 vim.opt.statusline = " %f %r %m %= [%l,%c] [%p%%] "
 vim.cmd [[
 	colorscheme notelight
+
+	let mapleader = ','
+	nmap <S-t>	:tabedit <CR>
+	nmap H		:tabprevious <CR>
+	nmap L		:tabnext <CR>
+	nmap <leader>f :NERDTreeToggle <CR>
+	
+	let g:floaterm_title = '($1/$2)'
+	let g:floaterm_width = 0.9
+	let g:floaterm_height = 0.9
+	let g:floaterm_keymap_new    = '<leader>k'
+	let g:floaterm_keymap_prev   = '<leader>h'
+	let g:floaterm_keymap_next   = '<leader>l'
+	let g:floaterm_keymap_toggle = '<leader>t'
 ]]
